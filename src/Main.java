26c42567995eb5aedbe223722ff5c787e26b4648
Koozak <diamentowyniko@gmail.com>
2023-09-05 11:41:55 +0200

    zgadywanie liczb z podpowiedziami

--- src/Main.java
+++ src/Main.java
@@ -18,34 +18,31 @@ public class Main {
          */
         System.out.println("Pierwszy program");
         System.out.println("wylosowanie jednej liczby z zakresu od 1 do 100");
-        int wylosowana =(int) (Math.random()*100+1);//  Math.random() losowanie z zakresu <0,1)
+        int wylosowana = (int) (Math.random() * 100 + 1);//  Math.random() losowanie z zakresu <0,1)
         //typy proste int,double,float,boolean,char
         //zmienne tych typów nie są obiektami
         //rzutowanie zawężające
         //z liczby która zajmuje więcej miejsca w pamieci robię liczbę ktora zajmuje miej miejsca w pamięci
-        System.out.println(wylosowana);
-        System.out.println("podaj liczbę całkowitą z zakresu 1,100");
-        Scanner klawiatura = new Scanner(System.in);
-        int wpisana;
-        wpisana = klawiatura.nextInt();
-        if(wpisana == wylosowana){
-            System.out.println("wygrałeś");
-        }
-        else{
-            System.out.println("nie udało sie");
-
-        //wyrażenie warunkowe
-        int roznica = wylosowana > wpisana ? wylosowana - wpisana : wpisana - wylosowana;
-        //całokowita z dzieleniea
-        roznica = roznica/10;
-            System.out.println(
-                    switch (roznica) {
-                        case 0 -> "było blisko";
-                        case 1 -> "całkiem nieżle";
-                        default -> "sprubuj ponownie";
-                    }
-                );
-
+        //System.out.println(wylosowana);
+        for(int i = 0; i<10;i++) {
+            System.out.println("podaj liczbę całkowitą z zakresu 1,100");
+            Scanner klawiatura = new Scanner(System.in);
+            int wpisana;
+            wpisana = klawiatura.nextInt();
+            if (wpisana == wylosowana) {
+                System.out.println("wygrałeś");
+                break;
             }
+            else{
+                if(wpisana<wylosowana){
+                    System.out.println("za mało");
+                }
+                else {
+                    System.out.println("za dużo");
+                }
+            }
+            }
+        System.out.println("liczba to "+wylosowana);
         }
     }
+
