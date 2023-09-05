76043f32a1f83b411913cd31a10b36775d9db579
unknown <diamentowyniko@gmail.com>
2023-09-05 10:50:37 +0200

    losowan ie liczby rzeczywistej

--- src/Main.java
+++ src/Main.java
@@ -5,5 +5,20 @@ public class Main {
     }
      */
     public static void main(String[] args) {
-        System.out.println("Pierwszy program");    }
+        /*
+        brak dostępne w pakiecie
+        public dostępne wszędzie
+        private dostępne tylko w tej klasie
+        protected dostępne w tej klasie  w klasach dziedziczących w całym pakiecie
+         */
+        /*
+        static metoda może być wywołana bez obiektu
+         */
+        System.out.println("Pierwszy program");
+        System.out.println("wylosowanie jednej liczby z zakresu od 1 do 100");
+        double wylosowana = Math.random()*100+1;//  Math.random() losowanie z zakresu <0,1)
+        //typy proste int,double,float,boolean,char
+        //zmienne tych typów nie są obiektami
+        System.out.println(wylosowana);
+    }
 }
