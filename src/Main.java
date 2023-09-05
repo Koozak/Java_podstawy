42dbd6e24c1cd639ab8e756640fca28716f377b3
unknown <diamentowyniko@gmail.com>
2023-09-05 10:40:55 +0200

    wypisywanie informacji na ekranie

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    //Nazwa pliku taka sama jak nazwa klasy, Nazwa klasy dużą literą
+    /*public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+     */
+    public static void main(String[] args) {
+        System.out.println("Pierwszy program");    }
+}
